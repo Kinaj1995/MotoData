233070619b200d1649eee0a7304d1c303df64171
schiltej <janik.schilter@edu.teko.ch>
2022-10-15 12:44:46 +0200

    Server: New Sidebar + Added Modal

--- Server/app.py
+++ Server/app.py
@@ -5,7 +5,7 @@ import os
 
 UPLOAD_FOLDER = 'static/upload'
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -16,17 +16,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 ## ================================================= ##
 
 @app.route('/')
-@app.route('/home')
 def index():
    return render_template('index.html')
 	
    
-@app.route('/uploader', methods = ['GET', 'POST'])
+@app.route('/', methods = ['GET', 'POST'])
 def upload_file():
    if request.method == 'POST':
-      f = request.files['file']
-      f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-      return render_template('map.html')
-		
-if __name__ == '__main__':
-   app.run(debug = True)
+      file = request.files['file']
+      file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+      return render_template('index.html', file=filename)
--- Server/static/js/leaflet.js
+++ Server/static/js/leaflet.js
@@ -15,13 +15,17 @@ var controlLayers = L.control.layers(null, null, {
 
 // display Carto basemap tiles with light features and labels
 var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+  maxNativeZoom: 40,
+  maxZoom:40
 }).addTo(map); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
 controlLayers.addBaseLayer(light, 'Carto Light basemap');
 
 /* Stamen colored terrain basemap tiles with labels */
 var terrain = L.tileLayer('https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.png', {
-  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.'
+  attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
+  maxNativeZoom: 40,
+  maxZoom:40
 }); // EDIT - insert or remove ".addTo(map)" before last semicolon to display by default
 controlLayers.addBaseLayer(terrain, 'Stamen Terrain basemap');
 
