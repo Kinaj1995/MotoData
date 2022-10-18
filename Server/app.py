c760701adab99d41ff1911109582fd9e14799a5e
schiltej <janik.schilter@edu.teko.ch>
2022-10-18 15:28:35 +0200

    Server: Added dir_Listing

--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,6 @@
 from fileinput import filename
-from flask import Flask, render_template, request, url_for
+from importlib.metadata import files
+from flask import Flask, render_template, request, url_for, abort, send_file
 
 import os
 
@@ -11,18 +12,43 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 ## ================ WEBINTERFACE =================== ##
-## 
+##
 ##
 ## ================================================= ##
 
-@app.route('/')
-def index():
-   return render_template('index.html')
-	
-   
-@app.route('/', methods = ['GET', 'POST'])
+
+## ================ UPLOAD FILE =================== ##
+
+@app.route('/', methods=['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      file = request.files['file']
-      file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-      return render_template('index.html', file=filename)
+    if request.method == 'POST':
+        file = request.files['file']
+        
+        if os.path.isfile(os.path.join(app.config['UPLOAD_FOLDER'], file.filename)):
+            return 'A File with the same name already exists. Please choose another Filename.'
+
+        else:
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+            return render_template('index.html', file=filename)
+
+
+## ================ LIST FILES =================== ##
+
+def dir_listing():
+    
+    # Show directory contents
+    files = os.listdir(UPLOAD_FOLDER)
+    return files
+
+
+## ================ TEMPLATES =================== ##
+
+@app.route('/')
+def index():    
+    return render_template('index.html', files=dir_listing())
+
+
+
+## ================ START SERVER =================== ##
+if(__name__ == "__main__"):
+   app.run(debug=True, host="127.0.0.1")  # Server startparameter  
